92a6f8baf0c3b392639e2642ea5ab044fd515631
jose <jkmj007@gmail.com>
2015-06-03 02:09:51 +0530

    added dfd.py

---
+++ dfd.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+print "pgm bgns"
+
+if 3==2:
+    print "king"
+else:
+    print "FOol"
+    print "exit"
+for i in range(0,10):
+    print "valu of i is", i
+
+def wle(a,b):
+    x=0
+    while(x<10):
+    	print "val is",x
+    	x=x+1
+    return a+b
+
+s = wle(5,6)
+print s
+import math
+print math.sin(30)
